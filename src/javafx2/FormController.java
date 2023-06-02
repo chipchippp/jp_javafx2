4322cc3f5c390923ed1cbc1dedd76383b8452fa6
ACER <locnvth2209036@fpt.edu.vn>
2023-06-02 23:03:53 +0700

    manager student1

--- src/javafx2/FormController.java
+++ src/javafx2/FormController.java
@@ -1,19 +1,17 @@
-package javafxdb;
+package javafx2;
 
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import database.Connector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
-import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 public class FormController {
     public TextField txtName;
@@ -30,14 +28,23 @@ public class FormController {
             String name = txtName.getText();
             String email = txtEmail.getText();
             String tel = txtTel.getText();
-            for (Student s: HomeController.listStudents){
-                if(s.getName().equals(name))
-                    throw new Exception("Tên SV đã tồn tại");
-                if(s.getEmail().equals(email))
-                    throw new Exception("Email đã tồn tại");
-            }
             Student sv = new Student(name,email,tel);
-            HomeController.listStudents.add(sv);
+//            HomeController.listStudents.add(sv);
+            // add to db
+            Connection conn = new Connector().getConn();
+
+            // query
+//            Statement stt = conn.createStatement();
+//            String sql = "insert into students(name,email,tel) values('"+
+//                    sv.getName()+"','"+sv.getEmail()+"','"+sv.getTel()+"')";
+//            stt.executeUpdate(sql);
+            String sql = "insert into students(name,email,tel) values(?,?,?)";
+            PreparedStatement stt = conn.prepareStatement(sql);
+            stt.setString(1,sv.getName());
+            stt.setString(2,sv.getEmail());
+            stt.setString(3,sv.getTel());
+            stt.executeUpdate();
+
             backToList(null);
         }catch (Exception e){
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -45,12 +52,4 @@ public class FormController {
             alert.show();
         }
     }
-       public void clear() {
-                txtName.clear();
-                txtEmail.clear();
-                txtTel.clear();
-            }
 }
-
-
-
