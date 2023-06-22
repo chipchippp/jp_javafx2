97fd9efa7a51236dd6bbd46eef9bc7593f6fa2cb
Nguyễn Văn Lộc <locnvth2209036@fpt.edu.vn>
2023-06-22 16:45:54 +0700

    exam paper

--- src/exampaper/Main.java
+++ src/exampaper/Main.java
@@ -5,15 +5,14 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     private static Map<String, Customer> customerMap = new HashMap<>();
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
         int select=0;
 
 
         do {
-            System.out.println("------------------------");
+
             System.out.println("Menu: ");
             System.out.println("1. Add new customer ");
             System.out.println("2. Find by name");
@@ -21,18 +20,12 @@ public class Main {
             System.out.println("4. Exit");
             select = sc.nextInt();
             sc.nextLine();
-            if (select == 1){
-                System.out.println("Import name: ");
-                String name = sc.nextLine();
-                System.out.println("Import email: ");
-                String email = sc.nextLine();
-                System.out.println("Import phone: ");
-                String phone = sc.nextLine();
-                Customer customer = new Customer(name, email, phone);
-
-                if (select == 2){
-
-                }
+            if (select == 1) {
+                addCustomer();
+            } else if (select == 2){
+                    findCustomerByName();
+            } else if (select ==3){
+                    displayAllCustomers();
             }
 
 
@@ -42,14 +35,28 @@ public class Main {
 
         }while (select!=4);
     }
-    private static void findCustomerByName(Scanner scanner) {
-        System.out.print("Enter name to search: ");
-        String name = scanner.nextLine();
 
+    private static void addCustomer(){
+        System.out.println("Import name: ");
+        String name = sc.nextLine();
+        System.out.println("Import email: ");
+        String email = sc.nextLine();
+        System.out.println("Import phone: ");
+        String phone = sc.nextLine();
+        Customer customer = new Customer(name, email, phone);
+
+        if (customer != null){
+            customerMap.put(name, customer);
+            System.out.println();
+        }
+    }
+    private static void findCustomerByName() {
+        System.out.print("Enter name to search: ");
+        String name = sc.nextLine();
         Customer customer = customerMap.get(name);
         if (customer != null) {
+            System.out.println("CRM");
             System.out.println("Name: " + customer.getName());
-            System.out.println("Email: " + customer.getEmail());
             System.out.println("Phone: " + customer.getPhone());
         } else {
             System.out.println("Not found.");
